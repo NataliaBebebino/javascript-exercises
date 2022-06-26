e1e086758777223e12dfbfb89cd06e7c0c769632
NataliaBebebino <nbebebino@gmail.com>
2022-06-26 17:23:49 -0400

    Practice JavaScript fundamentals

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
 const add = function() {
-	
+  let result = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    result += arguments[index];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  array.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  array.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 1;
+
+  while (exponent >=1) {
+    result *= base;
+    exponent--;
+  }
+  
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+
+  for (let index = 1; index <= number; index++) {
+    result *= index;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
